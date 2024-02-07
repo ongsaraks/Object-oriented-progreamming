a726d844a3f1efc469e9b63ea71263794f28bb78
ONGSA RAKSALAM <ongsaraks@gmail.com>
2024-02-07 21:59:20 +0700

    Fixed

--- Lab04_code_starter/SongApp.java
+++ Lab04_code_starter/SongApp.java
@@ -8,7 +8,7 @@ public class SongApp {
 		myPlaylist.addSongAtIndex(new Song("End Game", 4.11), 0);
 		myPlaylist.addSongAtIndex(new Song("Anywhere", 3.35), 2);
 		myPlaylist.showPlaylist();
-//	
+	
 //		System.out.println("\nRemove songs -----------------------");
 //		myPlaylist.removeSongByTitle("End Game");
 //		myPlaylist.removeSongByIndex(2);
@@ -17,14 +17,14 @@ public class SongApp {
 		System.out.println("\nCheck error ------------------------");
 		System.out.print("Add duplicate song: ");
 		myPlaylist.addSong(new Song("Perfect", 3.38));
-//		
-		System.out.print("Add song at invalid index (at most index now is 2): ");
-		myPlaylist.addSongAtIndex(new Song("Find you", 3.38), 3);
+		
+//		System.out.print("Add song at invalid index (at most index now is 2): ");
+//		myPlaylist.addSongAtIndex(new Song("Find you", 3.38), 3);
 //		System.out.print("Remove not found song: ");
 //		myPlaylist.removeSongByTitle("Find you");
 //		System.out.print("Remove song at invalid index: ");
 //		myPlaylist.removeSongByIndex(3);
-//		
+		
 		
 		// Uncomment these for bonus
 //		System.out.println("\n**************** BONUS ***************");
