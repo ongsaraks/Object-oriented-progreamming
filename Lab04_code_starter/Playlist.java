1d86dc0b90dee212879924a2eea26d1772fceb0e
ONGSA RAKSALAM <ongsaraks@gmail.com>
2024-02-08 07:27:52 +0700

    idk

--- Lab04_code_starter/Playlist.java
+++ Lab04_code_starter/Playlist.java
@@ -8,38 +8,50 @@ public class Playlist {
 	 this.songs = new ArrayList<Song>();
 	
 	}
-	public boolean addSong(Song song) {
-		 if (!songs.contains(song)) {
-			 	songs.add(song);
-	            return true;
-	      }
-		 else{
-	            return false;
-	      }
-		 
-		
-	}
-	public boolean addSongAtIndex(Song song, int index) {
-		 if (!songs.contains(song)) {
-			 	songs.add(index,song);
-	            return true;
-	      }
-		 else{
-	            return false ;
-	      }
-	}
-	public boolean removeSongByIndex(int index) {
-		 
-	}
-	publicSong removeSongByTitle(String title) {
-		 
-	}
-	public double getPlaylistDuration() {
-		
-	}
-	public ArrayList<Song> getTooLongSongs (double minute){
-	
-	}
+//	public boolean addSong(Song song) {
+//
+//	}
+//	public boolean addSongAtIndex(Song song, int index) {
+//		if(index < 0 || index > songs.size()) {
+//			System.out.println("Invalid index");
+//		}
+//		if (!songs.contains(song)) {
+//				songs.add(index,song);
+//	            return true;
+//		 }
+//		 else{
+//			 	System.out.println(song.getTitle() + "already in the playlist. ");
+//			 	return false;
+//	      }
+//	
+//	}
+//	public boolean removeSongByIndex(int index) {
+//		 if(index < 0 || index > songs.size()) {
+//				System.out.println("Invalid index");
+//				return false;
+//			}
+//		 else {
+//			 songs.remove(index);
+//			 return true;
+//		 }
+//	}
+//    Song removeSongByTitle(String title) {
+//        for (int i = 0; i < songs.size(); i++) {
+//            if (songs.get(i).getTitle().equals(title)) {
+//                return songs.remove(i);
+//            }
+//        }
+//        System.out.println("Remove not found song: Not found.");
+//        return null;
+//    }
+//    public double getPlaylistDuration() {
+//
+//    }
+//
+//    public ArrayList<Song> getTooLongSongs(double minute) {
+//
+//    }
+
 	public void showPlaylist() {
 		System.out.println(playlistName);
 		int j = 0;
