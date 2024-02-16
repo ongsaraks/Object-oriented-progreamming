a238943a09652891da939bf7a4bfdb1242f7fae9
ONGSA RAKSALAM <ongsaraks@gmail.com>
2024-02-17 02:19:47 +0700

    set up lab 6

---
+++ lab06_starter/AgeRestrictedProduct.java
@@ -0,0 +1,47 @@
+/**
+ * 
+ * Modify this class to inherit from Product class
+ *
+ */
+
+public class AgeRestrictedProduct {
+	private int minimumAge;
+
+	// TODO 1: constructor
+	public AgeRestrictedProduct(String name, double price, int minAge) {
+		// YOUR CODE HERE
+		//
+		//
+		System.out.println("Age-restricted product is created ...");
+	}
+	
+	// TODO 2: setter and getter
+	public void setMinimumAge(int age) {
+		// YOUR CODE HERE
+		//
+		//
+	}
+	
+	public int getMinimumAge() {
+		// YOUR CODE HERE
+		//
+		//
+	}
+	
+	// TODO 3: check eligibility 
+	public boolean isEligible(int age) {
+		// YOUR CODE HERE
+		//
+		//
+		return false;
+	}
+	
+	// TODO 4: toString method
+	@Override
+	public String toString() {
+		// YOUR CODE HERE
+		//
+		//
+		return null;
+	}
+}
---
+++ lab06_starter/ShoppingCart.java
@@ -0,0 +1,72 @@
+
+import java.util.ArrayList;
+
+public class ShoppingCart {
+	
+	private ArrayList<Product> products;
+	private int customerAge;
+	
+	
+	public ShoppingCart(int customerAge) {
+		this.products = new ArrayList<Product>();
+		this.customerAge = customerAge;
+		System.out.println("Shopping Cart is created ... ");
+	}
+	
+	public void setCustomerAge(int age) {
+		this.customerAge = age;
+	}
+	
+	public int getCustomerAge() {
+		return this.customerAge;
+	}
+	
+	// TODO 5: add product
+	public void addProduct(Product product) {
+		// YOUR CODE HERE
+		//
+		//
+	}
+	
+	// TODO 6: calculate total price
+	public double calculateTotalPrice() {
+		// YOUR CODE HERE
+		//
+		//
+		return 0.0;
+	}
+
+	public void clear() {
+		products.clear();
+		System.out.println("Shopping Cart is cleared ... ");
+	}
+	
+	
+	// DO NOT MODIFY MAIN
+	public static void main(String[] args) {
+		System.out.println("Creating Products");
+		Product candy = new Product("Candy", 25.00);
+		AgeRestrictedProduct wine = new AgeRestrictedProduct("Wine", 999, 21);
+		System.out.println("------------------------------------------\n");
+		
+		ShoppingCart cart = new ShoppingCart(20);
+		cart.addProduct(candy);
+		cart.addProduct(wine);
+		
+		System.out.println("\nChecking out (Age:" + cart.getCustomerAge()+ ")");
+		System.out.println("Total price: " + cart.calculateTotalPrice());
+		System.out.println("------------------------------------------\n");
+		
+		
+		cart.clear();				// clear shopping cart (remove all products in the cart)
+		cart.setCustomerAge(35);	// change the customer age to 35 years old
+		cart.addProduct(candy);
+		cart.addProduct(wine);
+		
+		System.out.println("\nChecking out (Age:" + cart.getCustomerAge() + ")");
+		System.out.println("Total price: " + cart.calculateTotalPrice());
+		System.out.println("------------------------------------------\n");
+		
+	}
+
+}
