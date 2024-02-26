154389a978edd71fb177e2f764598b90a46e8672
ONGSA RAKSALAM <ongsaraks@gmail.com>
2024-02-26 23:57:35 +0700

    Create abstracclass

---
+++ Lab07-starter/ShapeAbstract.java
@@ -0,0 +1,17 @@
+
+abstract class ShapeAbstract {
+	private String color;
+	public ShapeAbstract(String color) {
+		this.color = color;
+	}
+	public String getColor() {
+		return color;
+	}
+	public void setColor(String color) {
+		this.color = color;
+	}
+	public String toString() {
+		return "Shape[color=" + color + "]";
+	}
+	abstract double gerArea();
+}
