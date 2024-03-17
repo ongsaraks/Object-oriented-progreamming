aa56b9e9730feb34a8cfa42128cc3b1764a2eaf7
ONGSA RAKSALAM <ongsaraks@gmail.com>
2024-03-17 14:05:38 +0700

    Create SortedQueue

---
+++ Lab9/SortedQueue.java
@@ -0,0 +1,31 @@
+
+public class SortedQueue<T extends Comparable<T>> {
+	public SortedQueue() {
+		
+	}
+	public SortedQueue(int capacity) {
+		
+	}
+	public boolean enqueue(int capacity) {
+		return false;
+	}
+	public T dequeue() {
+		return null;
+		
+	}
+	public T peek() {
+		return null;
+		
+		
+	}
+	public T[] toArray() {
+		return null;
+		
+	}
+	public String toString() {
+		return null;
+		
+	}
+	
+	
+}
