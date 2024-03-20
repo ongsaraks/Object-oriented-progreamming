fa86a104178dedebb044ccb2ad7a604e07ccf766
ONGSA RAKSALAM <ongsaraks@gmail.com>
2024-03-20 21:12:47 +0700

    Done lab9 with Biggest WTF

--- Lab9/SortedQueue.java
+++ Lab9/SortedQueue.java
@@ -2,29 +2,32 @@ import java.util.*;
 public class SortedQueue<T extends Comparable<T>> {
 		int capacity;
 		ArrayList<T> queue;
-	public SortedQueue() {
+	public SortedQueue() {	
 		queue = new ArrayList<>();
+		this.capacity = Integer.MAX_VALUE;
 	}
 	public SortedQueue(int capacity) {
 		this.capacity = capacity;
-		queue = new ArrayList<>(capacity);
+		queue = new ArrayList<>(this.capacity);
 	}
 	public boolean enqueue(T object) {
-		if(queue.size() >= this.capacity) {
+		 if (queue.size() >= capacity) {
 			return false;
 		}
-		queue.add(object);
-		Collections.sort(queue);
-		return true;
+			queue.add(object);
+			Collections.sort(queue);
+			return true;
 	
 
 	}
 	public T dequeue() {
 		if(queue.isEmpty()) {
 			return null;
+			
 		}
-		queue.remove(0);
-		return queue.get(0);
+		return  queue.remove(0);
+	
+		
 
 		
 	}
@@ -37,14 +40,30 @@ public class SortedQueue<T extends Comparable<T>> {
 
 
 	}
+	@SuppressWarnings("unchecked")
 	public T[] toArray() {
-		return null;
+		
+		T[] array = (T[]) new Comparable[queue.size()];
+        for (int i = 0; i < queue.size(); i++) {
+            array[i] = (T) queue.get(i);
+        }
+        return array;
+		
 
 	}
 	
 	public String toString() {
-		return null;
+		String str = "||";
+		for(int i = 0; i < queue.size();i++) {
+			str += queue.get(i);
+		if(queue.size() > 1 && i < queue.size()-1) {
+			str += "->";
+		}
+		}
+		str+= "||";
+		return str;
+		}
+		
 
 		
 	}
-}
