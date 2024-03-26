8c5412224d2a7f1b8a78402dfd5d9663e5a43eed
ONGSA RAKSALAM <ongsaraks@gmail.com>
2024-03-26 23:09:14 +0700

    add name

--- Lab10/Object3D.java
+++ Lab10/Object3D.java
@@ -1,13 +1,13 @@
-// File: <NAME OF FILE>
+// File: Object3D
 // Description: <A DESCRIPTION OF YOUR PROGRAM>
-// Assignment Number: <e.g., 1 – 13>
+// Assignment Number: 10
 //
-// ID: <YOUR STUDENT ID>
-// Name: <YOUR FULLNAME>
-// Section: <YOUR SECTION e.g., 1, 2, or 3>
+// ID: 6688093
+// Name: Ongsa Raksalam
+// Section: 2
 // Grader: <YOUR GRADER’S NAME>
 //
-// On my honor, <YOUR FULLNAME>, this lab assignment is my own work
+// On my honor, Ongsa, this lab assignment is my own work
 // and I have not provided this code to any other students.
 
 
