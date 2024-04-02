1ce2486acc56233adead99d16cf92d2da83f1601
ONGSA RAKSALAM <ongsaraks@gmail.com>
2024-04-02 12:20:14 +0700

    LOL

--- Lab11/ServiceLogProcessor.java
+++ Lab11/ServiceLogProcessor.java
@@ -15,28 +15,40 @@ public class ServiceLogProcessor {
 
     public void loadServiceRecords(String filename) {
         // TODO: Implement this method to read from the file and populate serviceRecords
-//    	BufferedReader reader = null;
-//    	File file = new File("C:/Users/gifly/Desktop/Object-oriented-progreamming/Lab11/"+filename);
-//    	try {
-//    		reader = new BufferedReader(new InputStreamReader(new FileInputStream(file)));
-//    	}catch(FileNotFoundException e) {
-//    		e.printStackTrace();
-//    	}catch(IOException e) {
-//    		e.printStackTrace();
-//    	}finally {
-//    		try {
-//    			if(reader != null) {
-//    				reader.close();
-//    			}
-//    		}catch(IOException e) {
-//    			e.printStackTrace();
-//    		}
-//    	}
+    	Scanner reader = null;
+//    	File file = new File("text/"+filename);
+    		try {
+        		reader = new Scanner(new File("text/"+filename));
+        		while(reader.hasNextLine()) {
+        			 String record = reader.nextLine().trim();
+        			 if (!record.isEmpty()) {
+        				 try {
+        					 validateRecord(record); // Validate each record
+                             serviceRecords.add(record);
+                             }
+        				 catch (InvalidServiceRecordException e) {
+                         // Log a warning to the console without stopping the program
+                         System.out.println("Warning: " + e.getMessage());
+                         errorLog.add(record);
+                         }
+        			}
+        		}
+        	}
+        	catch (FileNotFoundException e){
+        		e.printStackTrace();
+        	}
+        	
+        	finally {
+        			if(reader != null) reader.close();
+        	}
     	
     }
 
     private boolean validateRecord(String record) {
-        // TODO: Implement this method to validate individual service record
+        // TODO: Implement this method to validate individual service record4
+    	if(record < 0) {
+    		return false;
+    	}
         return true;
     }
 
